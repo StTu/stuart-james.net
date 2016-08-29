6dc887171fc82816089934bab19f568d618f2df7
Stuart James <stuart@stuart-james.net>
2016-08-29 23:04:02 +0100

    date

--- deploy.sh
+++ deploy.sh
@@ -3,8 +3,8 @@ mogrify -resize 200x -format png */*.png
 cpwd=pwd
 cd /vol/websites/stuart-james.net
 cd images/blog
-mogrify -resize 500x -format png */*.png
-mogrify -resize 500x -format jpg */*.jpg
+mogrify -resize 500x -format png *.png
+mogrify -resize 500x -format jpg *.jpg
 cd ../../
 git add *
 val=date
